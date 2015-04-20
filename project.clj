23ff3cd3fd80592fc20fa4eed861b8a00e2d8d4d
lsb <leebutterman@gmail.com>
2015-04-20 00:19:57 -0700

    bump version, give ourselves a bit more room

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject parse-wikipedia-revisions "0.1.0-SNAPSHOT"
+(defproject parse-wikipedia-revisions "0.2.0-SNAPSHOT"
   :description "Parse the Wikipedia stub-meta-history"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
   :main parse-wikipedia-revisions.core
-  :jvm-opts ["-Xmx512m" "-Xms512m" "-XX:+UseCompressedOops"]
+  :jvm-opts ["-Xmx3g" "-Xms3g"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.xml "0.0.8"]])
